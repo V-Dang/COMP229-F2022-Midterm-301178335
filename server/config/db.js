902527219f5b911108218cfd19909c5446c7d74a
V-Dang <vivian.dang96@hotmail.com>
2022-10-28 21:26:31 -0400

    mongodb atlas updated

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,6 @@
 module.exports = {
   //local MongoDB deployment ->
-  URI: "mongodb://127.0.0.1:27017/faculty_info",
+  //URI: "mongodb://127.0.0.1:27017/faculty_info",
+  URI: "mongodb+srv://admin:b9ZcsxNqqTZWOHCc@mongodbserver.7onmf9b.mongodb.net/faculty_info?retryWrites=true&w=majority"
+
 };
